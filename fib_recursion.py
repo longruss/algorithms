4bb42ad9d80efbcb35185361ba01bb107d12c555
longruss <43468727+longruss@users.noreply.github.com>
2018-10-24 19:50:23 +0300

    Update fib_recursion.py

--- fib_recursion.py
+++ fib_recursion.py
@@ -1,10 +1,14 @@
+import time
 num = input("Какое число найти? ")
+start_time = time.time()
 num = int(num)
 def fib(number):
     if number==1 or number==2:
         return 1
     return fib(number-1) + fib(number-2)
 print(str(num) + " число = " + str(fib(num)))
+
+print("--- %s seconds ---" % (time.time() - start_time))
 
 
 def main(args):
