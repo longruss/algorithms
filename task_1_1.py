4e747725964b9eb02e831a7de8254c2c4309fd26
longruss <43468727+longruss@users.noreply.github.com>
2018-10-25 11:52:52 +0300

    Update and rename task_1.1 to task_1_1.py

--- task_1_1.py
+++ task_1_1.py
@@ -11,16 +11,21 @@ def compare(array):
   perest = 0
   print(array)
   print()
+  #compare = 0
   for n in range(1,len(array)):
+    #if array[n] >= array[n-1]:
+      #compare += 1
     i = n - 1
     while (i > -1) and array[i+1] < array[i]:
         array[i+1], array[i] = array[i], array[i+1]
         i -= 1
-        #print(array)
+        print(array)
         perest += 1
   print()
   print("Количество перестановок = " + str(perest))
   print()
+  #print("Количество сравнений = " + str(perest + compare))
+  #print()
   print("Конечный массив = " + str(array))
   print()
 
